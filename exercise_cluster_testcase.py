6be8efff94360df8124559442b2d86fdca2b508c
Zach <zachbanks@icloud.com>
2022-01-12 21:23:01 -0500

    Added unit test framework for ExerciseCluster class.

---
+++ exercise_cluster_testcase.py
@@ -0,0 +1,18 @@
+from exercise_cluster import ExerciseCluster
+
+import unittest
+
+##############
+# UNIT TESTS #
+##############
+
+class ExerciseClusterTestCase(unittest.TestCase):
+    def test_str(self):
+        pass
+
+#############
+# RUN TESTS #
+#############
+
+if __name__ == '__main__':
+    unittest.main()
